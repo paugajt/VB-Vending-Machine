3204388cf4b02f6dd0ecd4b9b6d87b7498963a99
Justin Pauga <paugajt@gmail.com>
2017-09-12 10:57:30 -0600

    Add files via upload

---
+++ test1/test1/src/main/java/com/rebelscrum/test1/Test1Application.java
@@ -0,0 +1,12 @@
+package com.rebelscrum.test1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test1Application.class, args);
+	}
+}
